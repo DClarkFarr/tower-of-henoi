5e793db8ecbc621722e76f61381324d940df8437
Daniel Farr <dfarr@logitech.com>
2023-10-10 21:51:31 -0600

    lots of improvements

--- henoi/src/App.vue
+++ henoi/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 type Tower = number[];
 type TowerNum = 1 | 2 | 3;
 type Towers = Record<TowerNum, Tower>;
@@ -20,6 +20,8 @@ type QueueAction = {
 type Queue = QueueAction[];
 
 const DESTINATION: TowerNum = 3;
+
+const TOWERS: TowerNum[] = [1, 2, 3];
 /**
  * Native methods
  */
@@ -29,18 +31,7 @@ function createStartingDisks(numDiscs: number) {
     .fill(0)
     .map((v, k) => k);
 }
-function getTowerMax(towers: Towers, towerNum: TowerNum) {
-  const num = Math.max(...towers[towerNum]);
-  return isFinite(num) ? num : -1;
-}
-function getNextDiscCoords(towers: Towers): DiscCoords {
-  const tower1Max = getTowerMax(towers, 1);
-  const tower2Max = getTowerMax(towers, 2);
-  return {
-    tower: tower1Max > tower2Max ? 1 : 2,
-    disc: tower1Max > tower2Max ? tower1Max : tower2Max,
-  };
-}
+
 function moveDisc(
   towers: Towers,
   discCoords: DiscCoords,
@@ -51,27 +42,19 @@ function moveDisc(
   }
 
   const disc = towers[discCoords.tower].shift()!;
-  console.log(
-    "Execute: move disc ",
-    disc,
-    "from tower ",
-    discCoords.tower,
-    "to ",
-    toTowerNum
-  );
   towers[toTowerNum].unshift(disc);
 }
-function getTopDiscCoords(towers: Towers, towerNum: TowerNum) {
-  return {
-    tower: towerNum,
-    disc: towers[towerNum][0],
-  };
-}
+
 function isTopDisc(towers: Towers, towerNum: TowerNum, discNum: number) {
   return towers[towerNum][0] === discNum;
 }
 
-function executeQueueAction(towers: Towers, action: QueueAction, queue: Queue) {
+function executeQueueAction(
+  towers: Towers,
+  action: QueueAction,
+  queue: Queue,
+  actionsTaken: Queue
+) {
   moveDisc(towers, action.coords, action.to);
 
   queue.forEach((a) => {
@@ -79,6 +62,8 @@ function executeQueueAction(towers: Towers, action: QueueAction, queue: Queue) {
       a.coords.tower = action.to;
     }
   });
+
+  actionsTaken.push(action);
 }
 
 function canMoveDisc(
@@ -97,7 +82,12 @@ function canMoveDisc(
 function getDiscsToShift(towers: Towers, coords: DiscCoords) {
   return towers[coords.tower].filter((disc) => disc < coords.disc);
 }
-function applyNewShiftToQueue(towers: Towers, steps: number, queue: Queue) {
+function applyNewShiftToQueue(
+  towers: Towers,
+  steps: number,
+  queue: Queue,
+  actionsTaken: Queue
+) {
   const lastAction = queue[queue.length - 1];
   const discToShift = getDiscsToShift(towers, lastAction.coords);
 
@@ -108,83 +98,98 @@ function applyNewShiftToQueue(towers: Towers, steps: number, queue: Queue) {
   const shiftAlt = lastAction.to;
   const shiftDestination = getMoveAlt(lastAction.coords.tower, shiftAlt);
 
-  return handleQueueIteration(towers, steps, [
-    ...queue,
-    ...discToShift.reverse().map<QueueAction>((disc, i) => ({
-      action: "shift",
-      coords: {
-        tower: lastAction.coords.tower,
-        disc: disc,
-      },
-      to: i % 2 === 0 ? shiftDestination : shiftAlt,
-    })),
-  ]);
+  return handleQueueIteration(
+    towers,
+    steps,
+    [
+      ...queue,
+      ...discToShift.reverse().map<QueueAction>((disc, i) => ({
+        action: "shift",
+        coords: {
+          tower: lastAction.coords.tower,
+          disc: disc,
+        },
+        to: i % 2 === 0 ? shiftDestination : shiftAlt,
+      })),
+    ],
+    actionsTaken
+  );
 }
 
-function applyMicroShiftToQueue(towers: Towers, steps: number, queue: Queue) {
+function applyMicroShiftToQueue(
+  towers: Towers,
+  steps: number,
+  queue: Queue,
+  actionsTaken: Queue
+) {
   const lastAction = queue[queue.length - 1];
 
   const destination = lastAction.to;
 
-  const towersByMicroBase = ([1, 2, 3] as TowerNum[])
-    .map((towerNum) => {
-      const filtered = towers[towerNum].filter(
-        (n) => n < lastAction.coords.disc
-      );
-      return {
-        towerNum,
-        biggestBase: filtered.length ? Math.max(...filtered) : -1,
-      };
-    })
+  const towersByMicroBase = TOWERS.map((towerNum) => {
+    const filtered = towers[towerNum].filter((n) => n < lastAction.coords.disc);
+    return {
+      towerNum,
+      biggestBase: filtered.length ? Math.max(...filtered) : -1,
+    };
+  })
     .filter((base) => base.biggestBase > -1)
     .sort((a, b) => a.biggestBase - b.biggestBase);
 
-  if (towersByMicroBase[0].towerNum !== destination) {
-    throw new Error("towersByMicroBase was not destination. Rethink!");
-  }
-
-  const microDestination = towersByMicroBase[1].towerNum;
-  const microDisc = towersByMicroBase[1].biggestBase;
-  const miniDiscsToShift = getDiscsToShift(towers, {
+  let microDestination = towersByMicroBase[1].towerNum;
+  let microAlt = destination;
+  let microDisc = towersByMicroBase[1].biggestBase;
+  let miniDiscsToShift = getDiscsToShift(towers, {
     tower: destination,
     disc: microDisc,
   });
 
-  return handleQueueIteration(towers, steps, [
-    ...queue,
-    ...miniDiscsToShift.reverse().map<QueueAction>((disc, i) => ({
-      action: "shift",
-      coords: {
-        tower: destination,
-        disc: disc,
-      },
-      to:
-        i % 2 === 0
-          ? microDestination
-          : getMoveAlt(destination, microDestination),
-    })),
-  ]);
+  if (towersByMicroBase[0].towerNum !== microAlt) {
+    microDisc = towersByMicroBase[0].biggestBase;
+    microAlt = towersByMicroBase[0].towerNum;
+    miniDiscsToShift = getDiscsToShift(towers, {
+      tower: microAlt,
+      disc: microDisc + 1,
+    });
+  }
+
+  return handleQueueIteration(
+    towers,
+    steps,
+    [
+      ...queue,
+      ...miniDiscsToShift.reverse().map<QueueAction>((disc, i) => ({
+        action: "shift",
+        coords: {
+          tower: microAlt,
+          disc: disc,
+        },
+        to:
+          i % 2 === 0
+            ? microDestination
+            : getMoveAlt(microAlt, microDestination),
+      })),
+    ],
+    actionsTaken
+  );
 }
 function handleQueueIteration(
-  towersCopy: Towers,
+  towers: Towers,
   steps: number,
-  queue: Queue
-): number {
-  if (steps > 100) {
-    throw new Error("way to many steps, buddy");
-  }
+  queue: Queue,
+  actionsTaken: Queue
+): [number, Queue] {
   const lastAction = queue[queue.length - 1];
-
-  // console.log("start iteration", JSON.parse(JSON.stringify(queue)));
+  const towersCopy = JSON.parse(JSON.stringify(towers)) as Towers;
 
   if (canMoveDisc(towersCopy, lastAction.coords, lastAction.to)) {
     queue.pop();
-    executeQueueAction(towersCopy, lastAction, queue);
+    executeQueueAction(towersCopy, lastAction, queue, actionsTaken);
 
     if (queue.length) {
-      return handleQueueIteration(towersCopy, steps + 1, queue);
+      return handleQueueIteration(towersCopy, steps + 1, queue, actionsTaken);
     } else {
-      return steps + 1;
+      return [steps + 1, actionsTaken];
     }
   } else {
     if (lastAction.action === "move") {
@@ -192,39 +197,40 @@ function handleQueueIteration(
         isTopDisc(towersCopy, lastAction.coords.tower, lastAction.coords.disc)
       ) {
         // target tower is obstructed. Must do a micro shift.
-        return applyMicroShiftToQueue(towersCopy, steps, queue);
+        return applyMicroShiftToQueue(towersCopy, steps, queue, actionsTaken);
       } else {
-        return applyNewShiftToQueue(towersCopy, steps, queue);
+        return applyNewShiftToQueue(towersCopy, steps, queue, actionsTaken);
       }
     } else {
-      return applyMicroShiftToQueue(towersCopy, steps, queue);
+      return applyMicroShiftToQueue(towersCopy, steps, queue, actionsTaken);
     }
   }
-
-  return steps;
 }
 
 function getMoveAlt(currentTower: TowerNum, targetTower: TowerNum) {
-  return ([1, 2, 3] as TowerNum[]).filter(
+  return TOWERS.filter(
     (tower) => tower !== currentTower && tower !== targetTower
   )[0];
 }
 
-function makeMove(towers: Towers) {
-  // const nextDiscCoords = getNextDiscCoords(towers);
-
-  return handleQueueIteration(towers, 0, [
-    ...towers[1].map((disc) => {
-      return {
-        coords: {
-          tower: 1,
-          disc,
-        },
-        to: DESTINATION,
-        action: "move",
-      } as QueueAction;
-    }),
-  ]);
+function queueMoves(towers: Towers) {
+  return handleQueueIteration(
+    towers,
+    0,
+    [
+      ...towers[1].map((disc) => {
+        return {
+          coords: {
+            tower: 1,
+            disc,
+          },
+          to: DESTINATION,
+          action: "move",
+        } as QueueAction;
+      }),
+    ],
+    []
+  );
 }
 
 /**
@@ -232,6 +238,9 @@ function makeMove(towers: Towers) {
  */
 const numDiscs = ref(4);
 const totalMoves = ref(0);
+const minMoves = ref(0);
+const queue = ref<Queue>([]);
+const queueIndex = ref(-1);
 const towers = ref<Towers>({
   1: [],
   2: [],
@@ -239,12 +248,79 @@ const towers = ref<Towers>({
 });
 
 const towerHanoi = (numDiscs: number) => {
-  towers.value[1] = createStartingDisks(numDiscs);
+  queueIndex.value = -1;
+  queue.value = [];
+  towers.value = {
+    1: createStartingDisks(numDiscs),
+    2: [],
+    3: [],
+  };
+
+  const [steps, actionsTaken] = queueMoves(towers.value);
+  minMoves.value = steps;
+  queue.value = actionsTaken;
+};
+
+const onClickPlay = async () => {
+  const pluck = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        onClickNext();
+        resolve(null);
+      }, 750);
+    });
+  };
+  while (queueIndex.value < queue.value.length - 1) {
+    await pluck();
+  }
+};
+
+const onClickNextAll = () => {
+  while (queueIndex.value < queue.value.length - 1) {
+    onClickNext();
+  }
 };
 
 const onClickNext = () => {
-  totalMoves.value += makeMove(towers.value);
+  if (!queue.value.length) {
+    return;
+  }
+  if (queueIndex.value > queue.value.length - 1) {
+    queueIndex.value = -1;
+  }
+
+  queueIndex.value += 1;
+
+  const action = queue.value[queueIndex.value];
+
+  executeQueueAction(towers.value, action, queue.value, []);
 };
+
+const onChangeNumDiscs = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  const value = parseInt(target.value);
+  if (value < 3 || value > 10) {
+    return;
+  }
+  numDiscs.value = value;
+  towerHanoi(value);
+};
+
+const towersToRender = computed(() => {
+  const base: Towers = {
+    1: [],
+    2: [],
+    3: [],
+  };
+  (Object.keys(towers.value).map(Number) as TowerNum[]).forEach((towerNum) => {
+    base[towerNum] = Array(numDiscs.value - towers.value[towerNum].length).fill(
+      -1
+    );
+    base[towerNum].push(...towers.value[towerNum]);
+  });
+
+  return base;
+});
 
 onMounted(() => {
   towerHanoi(numDiscs.value);
@@ -255,60 +331,56 @@ onMounted(() => {
   <div id="app">
     <div class="left">
       <div class="towers">
-        <div class="tower tower-1">
+        <div
+          v-for="towerNum of TOWERS"
+          class="tower"
+          :class="[`tower-${towerNum}`]"
+          :key="`tower-${towerNum}`"
+        >
           <div
             class="disc"
             :class="[
               'disc-index-' + index,
               'disc-' + disc,
-              towers[1][index] !== undefined ? 'full' : 'empty',
+              towersToRender[towerNum][index] > -1 ? 'full' : 'empty',
             ]"
             v-for="(disc, index) in numDiscs"
             :key="`1-${index}`"
           >
-            <span class="disc-title" v-if="towers[1][index] !== undefined">
-              {{ towers[1][index] }}
+            <span
+              class="disc-title"
+              v-if="towersToRender[towerNum][index] > -1"
+            >
+              {{ towersToRender[towerNum][index] }}
             </span>
           </div>
-          <div class="tower-label">Tower<br />1</div>
-        </div>
-        <div class="tower tower-2">
-          <div
-            class="disc"
-            :class="[
-              'disc-index-' + index,
-              'disc-' + disc,
-              towers[2][index] !== undefined ? 'full' : 'empty',
-            ]"
-            v-for="(disc, index) in numDiscs"
-            :key="`2-${index}`"
-          >
-            <span class="disc-title" v-if="towers[2][index] !== undefined">
-              {{ towers[2][index] }}
-            </span>
-          </div>
-          <div class="tower-label">Tower<br />2</div>
-        </div>
-        <div class="tower tower-3">
-          <div
-            class="disc"
-            :class="[
-              'disc-index-' + index,
-              'disc-' + disc,
-              towers[3][index] !== undefined ? 'full' : 'empty',
-            ]"
-            v-for="(disc, index) in numDiscs"
-            :key="`3-${index}`"
-          >
-            <span class="disc-title" v-if="towers[3][index] !== undefined">
-              {{ towers[3][index] }}
-            </span>
-          </div>
-          <div class="tower-label">Tower<br />3</div>
+          <div class="tower-label">Tower<br />{{ towerNum }}</div>
         </div>
       </div>
       <div class="actions">
-        <button @click="onClickNext">Next Move</button>
+        <button @click="onClickNext" :disabled="queueIndex >= queue.length - 1">
+          Next Move
+        </button>
+        <button
+          @click="onClickNextAll"
+          :disabled="queueIndex >= queue.length - 1"
+        >
+          Next All
+        </button>
+        <button @click="onClickPlay" :disabled="queueIndex >= queue.length - 1">
+          Play Through
+        </button>
+      </div>
+      <div class="inputs">
+        <div class="form-group">
+          <label>Number of Discs</label>
+          <input
+            class="input"
+            :value="numDiscs"
+            type="number"
+            @input="onChangeNumDiscs"
+          />
+        </div>
       </div>
     </div>
     <div class="right">
@@ -317,8 +389,37 @@ onMounted(() => {
           <b>Total Moves: </b>
           <span>{{ totalMoves }}</span>
         </div>
-        <b>State </b>
-        <pre>{{ towers }}</pre>
+        <div>
+          <b>Min Moves: </b>
+          <span>{{ minMoves }}</span>
+        </div>
+        <div style="display: none">
+          <b>Current State </b>
+          <pre>{{ towers }}</pre>
+        </div>
+      </div>
+      <div class="queue">
+        <h4>Queue</h4>
+        <table class="table">
+          <tr>
+            <th>#</th>
+            <th>Disc</th>
+            <th>From Tower</th>
+            <th>To Tower</th>
+            <th>Taken</th>
+          </tr>
+          <tr
+            class="queue-disc"
+            v-for="(action, index) in queue"
+            :key="`${index}-${action.coords.disc}-${action.coords.tower}-${action.to}`"
+          >
+            <td>{{ index + 1 }}</td>
+            <td>{{ action.coords.disc }}</td>
+            <td>{{ action.coords.tower }}</td>
+            <td>{{ action.to }}</td>
+            <td>{{ index > queueIndex ? "No" : "Yes" }}</td>
+          </tr>
+        </table>
       </div>
     </div>
   </div>
@@ -333,13 +434,13 @@ body,
 html {
   padding: 0;
   margin: 0;
-  height: 100%;
+  min-height: 100%;
   font-family: sans-serif;
 }
 #app {
   display: flex;
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   background: black;
   padding: 10px;
   color: black;
@@ -379,6 +480,7 @@ html {
   color: #000;
   background: #ababab;
   width: 100%;
+  padding: 10px;
 }
 .tower-label {
   text-align: center;
@@ -387,5 +489,25 @@ html {
   border-top: solid 4px;
   margin-top: 25px;
   padding-top: 15px;
+}
+
+.queue {
+  .table {
+    th,
+    td {
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+    th {
+      font-size: 16px;
+      font-weight: 600;
+      min-width: 50px;
+      background: #eaeaea;
+    }
+    td {
+      text-align: center;
+      background: #eee;
+    }
+  }
 }
 </style>
